4649aeb075692d1242a32c56bd756b55764435ca
Erik Bystroň <erik.bystron@deizon.com>
2021-07-18 23:14:27 +0200

    [feature] Set SettingsValue as mongoDB document entity

--- src/main/java/com/deizon/services/model/settings/SettingsValue.java
+++ src/main/java/com/deizon/services/model/settings/SettingsValue.java
@@ -3,12 +3,14 @@ package com.deizon.services.model.settings;
 import com.deizon.services.model.Entity;
 import com.deizon.services.model.data.DataType;
 import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
+@Document(collation = "settings")
 public class SettingsValue extends Entity {
 
     private String settings;
